d34dba2522e11614755cb90973d1134ffb4e36ea
Sandeep Kumar <kumar.sandeep648@gmail.com>
2020-05-23 18:45:16 +0530

    added react-router-dom and added menu items withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import HatsPage from './pages/hatspage/hatspage.component'
+import { Route } from 'react-router-dom'
+ 
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path="/"  component={HomePage} />
+      <Route path="/shop/hats"  component={HatsPage} />
+
     </div>
   );
 }
---
+++ src/pages/hatspage/hatspage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const HatsPage = () => (
+    <div>
+    <h2>Hatspage</h2>
+        
+    </div>
+);
+
+export default HatsPage;
